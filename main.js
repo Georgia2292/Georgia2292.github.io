8d87018a3b05cd78f725252400b92601d6e4e1ae
Jorgelina <georgelina2292@gmail.com>
2024-08-18 00:23:54 -0500

    Actualización Landing Page

--- main.js
+++ main.js
@@ -1,1 +1,66 @@
-AbortController 
+
+const scrollRevealOption = {
+    distance: "50px",
+    origin: "bottom",
+    duration: 1000,
+  };
+  
+  // header container
+  ScrollReveal().reveal(".header__container img", {
+    duration: 1000,
+  });
+  
+  ScrollReveal().reveal(".header__container h1", {
+    ...scrollRevealOption,
+    delay: 500,
+  });
+  
+  ScrollReveal().reveal(".header__container p", {
+    ...scrollRevealOption,
+    delay: 1000,
+  });
+  
+  ScrollReveal().reveal(".header__btns", {
+    ...scrollRevealOption,
+    delay: 1500,
+  });
+  
+  // about container
+  ScrollReveal().reveal(".nosotros__image img", {
+    ...scrollRevealOption,
+    origin: "left",
+  });
+  
+  ScrollReveal().reveal(".nosotros__content h3", {
+    ...scrollRevealOption,
+    delay: 500,
+  });
+  
+  ScrollReveal().reveal(".nosotros__content .section__header", {
+    ...scrollRevealOption,
+    delay: 1000,
+  });
+  
+  ScrollReveal().reveal(".nosotros__content .section__subheader", {
+    ...scrollRevealOption,
+    delay: 1500,
+  });
+  
+  ScrollReveal().reveal(".nosotros__content .nosotros__grid", {
+    ...scrollRevealOption,
+    delay: 2000,
+  });
+  
+  // contact container
+  ScrollReveal().reveal(".contact__image img", {
+    ...scrollRevealOption,
+    origin: "left",
+  });
+  
+  const swiper = new Swiper(".swiper", {
+    loop: true,
+    slidesPerView: "auto",
+    centeredSlides: true,
+    spaceBetween: 30,
+  });
+  
